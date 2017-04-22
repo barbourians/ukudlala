8e79928fcfd18a9fc3ef71cd40af18eea901607e
Ian Barbour <barbourians@users.noreply.github.com>
2017-04-22 23:53:55 +0200

    Update birkbeck_intro_ex_a.py

--- birkbeck_intro_ex_a.py
+++ birkbeck_intro_ex_a.py
@@ -1,10 +1,9 @@
-. a Python instruction which would output a blank line.
-2. an instruction which would output
-Hickory, Dickory, Dock
-3. a program which reads in two words, one after the other, and then displays them
-in reverse order. For example, if the input was
-First
-Second
-the output should be
-Second
-First
+# 1. a Python instruction which would output a blank line.
+# 2. an instruction which would output: Hickory, Dickory, Dock
+# 3. a program which reads in two words, one after the other, and then displays them in reverse order.
+# For example, if the input was
+#   First
+#   Second
+# The output should be
+#   Second
+#   First
