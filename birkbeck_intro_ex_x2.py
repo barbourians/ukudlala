50e672a63851ade6a3000a387edabcbc4e1dda4d
Ian Barbour <barbourians@users.noreply.github.com>
2017-04-22 23:55:54 +0200

    Update birkbeck_intro_ex_x2.py

--- birkbeck_intro_ex_x2.py
+++ birkbeck_intro_ex_x2.py
@@ -1,4 +1,3 @@
-Exercise X2
-If you have worked out what the above program does, can you see that, for certain series
-of numbers, it will not produce the correct output? In what circumstances will it not
-work correctly, and how could you change the program to make it work properly?
+# If you have worked out what the above program does, can you see that, for certain series
+# of numbers, it will not produce the correct output? In what circumstances will it not
+# work correctly, and how could you change the program to make it work properly?
