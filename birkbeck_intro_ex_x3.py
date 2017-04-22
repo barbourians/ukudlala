a6e85b85aa0fc6ff4155d7c34c67439326377046
Ian Barbour <barbourians@users.noreply.github.com>
2017-04-22 23:56:46 +0200

    Update birkbeck_intro_ex_x3.py

--- birkbeck_intro_ex_x3.py
+++ birkbeck_intro_ex_x3.py
@@ -1,4 +1,3 @@
-Exercise X3
-Write a program that takes a series of numbers (ending in 0) and counts the number of
-times that the number 100 appears in the list. For example, for the series 2, 6, 100, 50,
-101, 100, 88, 0, it would output 2.
+# Write a program that takes a series of numbers (ending in 0) and counts the number of
+# times that the number 100 appears in the list.
+# For example, for the series 2, 6, 100, 50, 101, 100, 88, 0, it would output 2.
